43955b2378865a492a65f13c554982ef51998711
Dan Hintz <dhintz@live.com>
2019-04-07 19:19:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -38,6 +38,7 @@ class App < Sinatra::Base
       when "multiply" then (@num1 * @num2).to_s
       when "divide" then (@num1 / @num2).to_s
     else "Not a valid operation"
+    end
   end
         
   
